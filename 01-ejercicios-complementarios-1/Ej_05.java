4ff100db715b0e4bce00d6c5bf084ef515570a86
Rafa Villar <villarrafael@gmail.com>
2021-05-29 14:24:32 -0300

    update

--- 01-ejercicios-complementarios-1/Ej_05.java
+++ 01-ejercicios-complementarios-1/Ej_05.java
@@ -7,11 +7,15 @@ public class Ej_05{
         System.out.println("Ingrese dos números multiplicar por favor: ");
         int a = scan.nextInt();
         int b = scan.nextInt();
-        int s = suma(a, b);
-        System.out.println(a + " * " + b + " = " + s);
+        int m = multiplicar(a, b);
+        System.out.println(a + " * " + b + " = " + m);
         scan.close();
     }
-    public static int suma(int a, int b){
-        return a * b;
+    public static int multiplicar(int a, int b){
+        int m=0;
+        for(int i=1; i<=b; i++){
+            m+=a;
+        }
+        return m;
     }
 }
